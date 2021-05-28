d9309fadf56b279685f998973657175aab352261
AlwaysLee-Creater <54054460+AlwaysLee-Creater@users.noreply.github.com>
2021-05-28 11:03:32 +0900

    Feat: 좋아요, 싫어요 모델 생성

---
+++ server/models/Dislike.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+const moment = require("moment");
+const Schema=mongoose.Schema
+
+const dislikeSchema = mongoose.Schema({
+    userId:{
+        type:Schema.Types.ObjectId,
+        ref:'User'
+    },
+    commentId:{
+        type:Schema.Types.ObjectId,
+        ref:'Comment'
+    },
+    videoId:{
+        type:Schema.Types.ObjectId,
+        ref:'Video'
+    }
+},{timestamps:true})
+
+const Dislike = mongoose.model('Dislike', dislikeSchema);
+
+module.exports = { Dislike }
